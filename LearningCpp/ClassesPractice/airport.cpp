b83b3155598383bcab0678983e127d06161270cc
pforderique <porderiq@mit.edu>
2021-01-22 20:41:43 -0500

    error

--- LearningCpp/ClassesPractice/airport.cpp
+++ LearningCpp/ClassesPractice/airport.cpp
@@ -8,22 +8,24 @@ Airport structure will have several members
 - person : a person has a name, occupation
 */
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Person {
+protected:
     string firstname;
     string lastname;
     string fullname;
     string occupation;
 
 public:
-    Person(string firstname, string lastname = "", string occupation = "") 
+    Person(string occupation, string firstname = "", string lastname = "") 
         : firstname(firstname), lastname(lastname), occupation(occupation) 
     {
         fullname = firstname + " " + lastname;
     }
 
-    string toString() { return fullname + ", " + occupation;}
+    string toString() { return occupation + " " + fullname;}
 
     // getters and setters
     string getFirstName() { return firstname; }
@@ -32,18 +34,67 @@ public:
 
 };
 
-class Pilot {
+class Passenger {
 
 };
 
-class Plane {
+// class Pilot : public Person{
+//     Plane plane;
+//     int id;
 
-};
+// public:
+//     Pilot(Plane plane, string firstname="", string lastname="")
+//         : Person("pilot", firstname, lastname), plane(plane)
+//     {}
+// };
+
+// class CoPilot {
+
+// };
+
+// class FlightAttendant {
+
+// };
+
+// class Crew {
+//     Pilot pilot;
+//     CoPilot copilot;
+//     vector<FlightAttendant> attendants;
+// public:
+//     Crew(Pilot pilot, CoPilot copilot, vector<FlightAttendant> attendants) {
+
+//     }
+// };
+
+// class Plane {
+// protected:
+//     Crew crew;
+//     vector<Passenger> passengers;
+// public:
+//     Plane() {
+
+//     }
+// };
+
+// class DeltaPlane {
+
+// };
+
+// class SpiritPlane {
+
+// };
+
+// class AAPlane {
+
+// };
 
 // for testing
 int main(int argc, char const *argv[])
 {
-    Person person1("John", "Wick", "Accountant");
-    cout << person1.toString() << endl;
+    Person person1("Accountant", "John", "Wick");
+    // Plane plane1("707");
+    // Pilot pilot1(plane1, "Henry");
+    // cout << pilot1.toString() << endl;
+
     return 0;
 }
